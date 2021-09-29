cf907ad776e4298622789e41d7f31f928b1983be
himanshuchauhan2907 <himanshuchauhan2907@gmail.com>
2021-09-29 19:02:15 +0530

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -8,9 +10,67 @@ namespace StarChart.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public CelestialObjectController(ApplicationDbContext  context)
+        public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
         }
-    }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+
+            foreach (var obj in _context.CelestialObjects)
+            {
+                if (obj.Id != id)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    obj.Satellites = new System.Collections.Generic.List<CelestialObject>();
+                    obj.Satellites.Add(obj);
+                    return Ok(obj);
+                }
+            }
+            return Ok();
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+
+            var data = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+            if (data.Count==0)
+            {
+                return NotFound();
+            }                                                                                                                                              
+
+            foreach (var obj in _context.CelestialObjects)
+            {
+                if (_context.CelestialObjects.Where(x => x.OrbitedObjectId == obj.Id).Any())
+                {
+                    obj.Satellites = new System.Collections.Generic.List<CelestialObject>();
+                    obj.Satellites.Add(obj);
+                    
+                }
+               
+            }
+
+            return Ok(_context.CelestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            foreach (var obj in _context.CelestialObjects)
+            {
+               
+                    obj.Satellites = new System.Collections.Generic.List<CelestialObject>();
+                    obj.Satellites.Add(obj);
+
+            }
+
+            return Ok(_context.CelestialObjects);
+        }
+        }
 }
